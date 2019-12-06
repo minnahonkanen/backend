5b25c1309fb51e243ff4765cdba83d1a99517955
Minna Honkanen <autio.minna@gmail.com>
2019-12-06 15:43:11 +0200

    add frontend

---
+++ build/precache-manifest.d7b5635f8b95e73f8480d645f07f7be0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a192e23142a1ef26cb89ed3d2bd8c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e4025f50669eab5cd57",
+    "url": "/static/css/main.9ec3401d.chunk.css"
+  },
+  {
+    "revision": "6dcbf56918467efad26d",
+    "url": "/static/js/2.dc3e0949.chunk.js"
+  },
+  {
+    "revision": "9e4025f50669eab5cd57",
+    "url": "/static/js/main.6e74a0b1.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
---
+++ build/precache-manifest.f6f7de09741259c091c1bee5006a31f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58bd4ced975c27fbee19a5d7a87df34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ac4a3036ba1f0969edd",
+    "url": "/static/css/main.9ec3401d.chunk.css"
+  },
+  {
+    "revision": "6dcbf56918467efad26d",
+    "url": "/static/js/2.dc3e0949.chunk.js"
+  },
+  {
+    "revision": "5ac4a3036ba1f0969edd",
+    "url": "/static/js/main.ec273278.chunk.js"
+  },
+  {
+    "revision": "3da10799abf0dbd55beb",
+    "url": "/static/js/runtime-main.b1e82656.js"
+  }
+]);
